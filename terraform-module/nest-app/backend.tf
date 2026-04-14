90dcc52e4d500bcf70f89b3b43ef62fb2691d4c7
Tolani Akintayo <akintayotolani100@gmail.com>
2026-04-14 13:36:06 +0100

    edit: The key for the backend from ec2 to ecs

--- terraform-module/nest-app/backend.tf
+++ terraform-module/nest-app/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "dev-nest-terraform-state"
-    key            = "terraform-module/nest/ec2/terraform.tfstate"
+    key            = "terraform-module/nest/ecs/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "dev-nest-terraform-lock"
     profile        = "tolani-admin" # This profile name must be specified unless the terraform init would not work
